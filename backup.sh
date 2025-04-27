b15cd3de76b46325ee30955468f003cf069a183d
samy <anotherlurker@protonmail.com>
2025-04-27 14:10:36 +0200

    Copying and backing up complete

--- backup.sh
+++ backup.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
 
+echo "Defining variables..."
 # Define variables
 CONTAINER_NAME="minecraft-java-server"
-BACKUP_DIR="/media/samy/verbatim/backup/${CONTAINER_NAME}"
-TEMP_DIR="/media/samy/verbatim/project/minecraft-java/minecraft"
+BACKUP_DIR="/media/verbatim/samy/backup/${CONTAINER_NAME}"
+TEMP_DIR="/media/verbatim/samy/project/minecraft-java/backup/"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="${BACKUP_DIR}/minecraft_world_backup_${DATE}.tar.gz"
 RETENTION_DAYS=7
 
 # Copy the whole minecraft folder including worlds and server settings
 echo "Copying minecraft server folder from container..."
-docker cp ${CONTAINER_NAME}:/data/minecraft/world ${TEMP_DIR}
+docker cp ${CONTAINER_NAME}:/data/minecraft ${TEMP_DIR}
 
 # Create a backup 
 echo "Creating backup..."
