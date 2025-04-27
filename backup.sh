7627944922456bf119eefdf52f1bf4f4b899cbb8
samy <anotherlurker@protonmail.com>
2025-04-27 18:31:10 +0200

    Add the link

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@ echo "Defining variables..."
 CONTAINER_NAME="minecraft-java-server"
 BACKUP_DIR="/media/verbatim/samy/backup/${CONTAINER_NAME}"
 TEMP_DIR="/media/verbatim/samy/project/minecraft-java/backup/"
+LAST_BACKUP_LINK="${TEMP_DIR}latest_backup"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="${BACKUP_DIR}/minecraft_world_backup_${DATE}.tar.gz"
 RETENTION_DAYS=7
@@ -19,6 +20,10 @@ if tar -czf ${BACKUP_FILE} -C ${TEMP_DIR} minecraft; then
 
     # Delete old backups
     find ${BACKUP_DIR} -name "minecraft_world_backup_*.tar.gz" -mtime +${RETENTION_DAYS} -exec rm {} \;
+
+    # Update the latest backup link
+    rm ${LAST_BACKUP_LINK}
+    ln -s ${BACKUP_FILE} ${LAST_BACKUP_LINK}
 else
     echo "Backup failed!"
 fi
