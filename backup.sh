a4a3cdbcedf0a5ec6a9de6680eb6e34211f21809
samy rahakbauw <samy@suratsair.fritz.box>
2025-04-27 13:53:16 +0200

    Filled and bare minimum to execute

--- backup.sh
+++ backup.sh
@@ -3,44 +3,27 @@
 # Define variables
 CONTAINER_NAME="minecraft-java-server"
 BACKUP_DIR="/media/samy/verbatim/backup/${CONTAINER_NAME}"
-TEMP_DIR="/tmp/${CONTAINER_NAME}"
-NFS_SHARE="fileserver:/path/to/nfs/share"
-NFS_MOUNT_POINT="/mnt/nfs"
+TEMP_DIR="/media/samy/verbatim/project/minecraft-java/minecraft"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="${BACKUP_DIR}/minecraft_world_backup_${DATE}.tar.gz"
 RETENTION_DAYS=7
 
-# Copy the world directory from the container to the host
-echo "Copying world directory from container to host..."
-docker cp ${CONTAINER_NAME}:/data/world ${TEMP_DIR}
+# Copy the whole minecraft folder including worlds and server settings
+echo "Copying minecraft server folder from container..."
+docker cp ${CONTAINER_NAME}:/data/minecraft/world ${TEMP_DIR}
 
-# Create a backup of the world directory
+# Create a backup 
 echo "Creating backup..."
-if tar -czf ${BACKUP_FILE} -C ${TEMP_DIR} world; then
+if tar -czf ${BACKUP_FILE} -C ${TEMP_DIR} minecraft; then
     echo "Backup completed: ${BACKUP_FILE}"
 
-    # Mount the NFS share
-    echo "Mounting NFS share..."
-    mkdir -p ${NFS_MOUNT_POINT}
-    mount ${NFS_SHARE} ${NFS_MOUNT_POINT}
-
-    if [ $? -eq 0 ]; then
-        # Copy the backup to the NFS share
-        echo "Copying backup to NFS share..."
-        cp ${BACKUP_FILE} ${NFS_MOUNT_POINT}/
-
-        # Unmount the NFS share
-        echo "Unmounting NFS share..."
-        umount ${NFS_MOUNT_POINT}
-
-        # Delete old backups
-        find ${BACKUP_DIR} -name "minecraft_world_backup_*.tar.gz" -mtime +${RETENTION_DAYS} -exec rm {} \;
-    else
-        echo "Failed to mount NFS share!"
-    fi
+    # Delete old backups
+    find ${BACKUP_DIR} -name "minecraft_world_backup_*.tar.gz" -mtime +${RETENTION_DAYS} -exec rm {} \;
 else
     echo "Backup failed!"
 fi
 
+# copy 
+
 # Clean up temporary directory
-rm -rf ${TEMP_DIR}/world
+#rm -rf ${TEMP_DIR}/world
